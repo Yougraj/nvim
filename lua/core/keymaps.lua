0afee11f78068b46c6bd1a0da44b7bd59dd33e56
Yougraj <yougrajbora.developer@gmail.com>
2023-08-14 18:40:47 +0530

    changing the keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = " "
-local keymap = vim.keymap 
+local keymap = vim.keymap
 keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
 
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -13,10 +12,10 @@ keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "Nzzzv")
 
 keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
+	require("vim-with-me").StartVimWithMe()
 end)
 keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
+	require("vim-with-me").StopVimWithMe()
 end)
 
 -- clear search highlight
@@ -24,15 +23,15 @@ keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 keymap.set("n", "x", '"_x"')
 
-
+keymap.set("n", "w", ":w<CR>")
 -- greatest remap ever
 keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever
-keymap.set({"n", "v"}, "<leader>y", [["+y]])
+keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 keymap.set("n", "<leader>Y", [["+Y]])
 
-keymap.set({"n", "v"}, "<leader>d", [["_d]])
+keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 keymap.set("i", "jj", "<Esc>")
@@ -49,35 +48,29 @@ keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-
 keymap.set("n", "<C-t>", "<cmd>NvimTreeToggle<cr>")
 
 keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+	vim.cmd("so")
 end)
-
 
 keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-
-
-
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
 
-
 -- NvimTree
 keymap.set("n", "<C-n>", function()
-    vim.cmd("NvimTreeToggle")
+	vim.cmd("NvimTreeToggle")
 end)
 
 keymap.set("n", "<leader>e", function()
-    vim.cmd("NvimTreeFocus")
+	vim.cmd("NvimTreeFocus")
 end)
 
 -- telescope
@@ -86,4 +79,3 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
- 
